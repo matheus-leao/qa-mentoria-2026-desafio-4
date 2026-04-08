ba5d3fbdefd194896fec6eda2b278c6bd34f6339
Matheus Leao <matsouzaleao@gmail.com>
2026-04-08 17:02:46 -0300

    Ajustando referencia do app e criando um builder para facilitar o acesso

--- test/api/health/get.test.js
+++ test/api/health/get.test.js
@@ -1,15 +1,15 @@
 import request from 'supertest';
 import { expect } from 'chai';
-import app from '../../../app.js';
+import { getApp } from '../helpers/appBuilder.js';
 
 describe('GET /health', () => {
   it('retorna 200', async () => {
-    const res = await request(app).get('/health');
+    const res = await request(getApp()).get('/health');
     expect(res.status).to.equal(200);
   });
 
   it('retorna JSON com indicador de saúde', async () => {
-    const res = await request(app)
+    const res = await request(getApp())
       .get('/health')
       .expect('Content-Type', /json/);
 
