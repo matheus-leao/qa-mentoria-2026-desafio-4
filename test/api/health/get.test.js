65063300c7c99b9f65ca8827b477275518506ba9
Matheus Leao <matsouzaleao@gmail.com>
2026-04-08 16:59:40 -0300

    Corrigindo referencia do app

--- test/api/health/get.test.js
+++ test/api/health/get.test.js
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import { expect } from 'chai';
-import app from '../../app.js';
+import app from '../../../app.js';
 
 describe('GET /health', () => {
   it('retorna 200', async () => {
