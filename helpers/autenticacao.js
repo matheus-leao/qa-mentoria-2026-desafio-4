d6ee97dbe3416e1ee835b2c49b86b668886dfe6b
Hugo Torquetti <112561756+HugoTorquetti@users.noreply.github.com>
2026-04-12 18:18:07 -0300

    Apply suggestion from @matheus-leao
    Boa. Tem razão. Não me atentei a esse detalhe.
    
    Co-authored-by: Matheus Leão <matsouzaleao@gmail.com>

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 //const request = require('supertest');
 
-export const obterToken = async (app, id, senha) => {
+export const obterToken = async (app, id_funcionario, senha) => {
     const respostaLogin = await request(app)
         .post('/auth/admin/login')
         .set('Content-Type', "application/json")
