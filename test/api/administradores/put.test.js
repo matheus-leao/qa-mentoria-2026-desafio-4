454f63860ef0541043d2c55278ca71255148fb6a
nascimentoluana <167343668+nascimentoluana@users.noreply.github.com>
2026-04-13 11:45:47 +0100

    CR changes

--- test/api/administradores/put.test.js
+++ test/api/administradores/put.test.js
@@ -10,18 +10,19 @@ describe('PUT /administradores/:id', () => {
     })
 
     it('Deve retornar 200 ao editar o cadastro de um administrador', async () => {
-        const response = await request(getApp()).put('/administradores/2')
-        .send({
+        const bodyPut = {
                 'nome': 'Teste',
                 'sobrenome': 'Nascimento',
                 'id_funcionario': 10,
                 'email': 'teste@example.com',
                 'senha': '1234567'
-            })
+        }
+        const response = await request(getApp()).put('/administradores/2')
+        .send(bodyPut)
         expect(response.status).to.equal(200);
-        expect(response.body.nome).to.equal('Teste')
-        expect(response.body.sobrenome).to.equal('Nascimento')
-        expect(response.body.id_funcionario).to.equal(10)
-        expect(response.body.email).to.equal('teste@example.com')
+        expect(response.body.nome).to.equal(bodyPut.nome)
+        expect(response.body.sobrenome).to.equal(bodyPut.sobrenome)
+        expect(response.body.id_funcionario).to.equal(bodyPut.id_funcionario)
+        expect(response.body.email).to.equal(bodyPut.email)
     })
 })
