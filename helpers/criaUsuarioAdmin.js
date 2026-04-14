d0dfbabdec18df9ee890838461a8a008d9dd84e4
Hugo Torquetti <112561756+TorksCoder@users.noreply.github.com>
2026-04-13 22:37:31 -0300

    adicionei o helper cadastraLivro para que ao iniciar a suíte já tenhamos um livro cadastrado na memória e também a fixture usuarioAdmin.json para passar como parametro no helper criaUsuarioAdmin e no teste pode comparar o objeto completo sem precisar fazer linha por linha. Feitas também outras correções como titulos dos testes, remoção de código comentado e remoção de espaços vazios.

--- helpers/criaUsuarioAdmin.js
+++ helpers/criaUsuarioAdmin.js
@@ -1,16 +1,15 @@
 import request from 'supertest';
-//const request = require('supertest');
 
-export const usuarioAdmin = async (app, nome, sobrenome, id, email, senha) => {
+export const criaUsuarioAdmin = async (app, usuarioAdmin) => {
     const admin = await request(app)
         .post('/administradores')
         .set('Content-Type', 'application/json')
         .send({
-            'nome': nome,
-            'sobrenome': sobrenome,
-            'id_funcionario': id,
-            'email': email,
-            'senha': senha
+            'nome': usuarioAdmin.nome,
+            'sobrenome': usuarioAdmin.sobrenome,
+            'id_funcionario': usuarioAdmin.id_funcionario,
+            'email': usuarioAdmin.email,
+            'senha': usuarioAdmin.senha
         });
 
     return admin;
--- test/cadastroLivros.test.js
+++ test/cadastroLivros.test.js
@@ -1,36 +1,37 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import { getApp } from './api/helpers/appBuilder.js';
-//import app from '../app.js';
-import { usuarioAdmin } from '../helpers/criaUsuarioAdmin.js';
+import { criaUsuarioAdmin } from '../helpers/criaUsuarioAdmin.js';
 import { obterToken } from '../helpers/autenticacao.js';
+import { cadastrarLivroValido } from '../helpers/cadastraLivro.js';
 import livro from '../fixtures/postLivros.json' with { type: 'json' };
-
+import usuarioAdmin from '../fixtures/usuarioAdmin.json' with { type: 'json' };
 
 describe('Testes de Cadastro de Livros', () => { 
     let admin;
     let token;
+    let livroCadastrado;
 
     before(async () => {
-        admin = await usuarioAdmin(getApp(), 'Admin', 'Teste', '10', 'admin@teste.com', 'senha123');
+        admin = await criaUsuarioAdmin(getApp(), usuarioAdmin);
         token = await obterToken(getApp(), admin.body.id_funcionario, 'senha123');
+        livroCadastrado = await cadastrarLivroValido(getApp(), token, livro);
     });
 
 
     describe('GET /cadastroLivros', () => {
-        it ('BUsca a lista de Livros cadastrados', async () => {
+        it('BUsca a lista de Livros cadastrados', async () => {
             const resposta = await request(getApp())
             .get('/livros')
             .set('Authorization', `Bearer ${token}`);
 
             expect(resposta.status).to.equal(200);
-        
         });
     });
 
     describe('POST /Livros', () => {
-        it.skip('Cria um novo registro livro e retorna 201 ', async () => {
-            const livroValido = { ...livro, id_livro: '201S' };
+        it.skip('Cadastra um novo livro e retorna 201 ', async () => {
+            const livroValido = { ...livroCadastrado, id_livro: '201S' };
             //Bug rastreado em: https://github.com/matheus-leao/qa-mentoria-2026-desafio-4/issues/23#issue-4249456071
             const resposta = await request(getApp())
             .post('/livros')
@@ -38,20 +39,11 @@ describe('Testes de Cadastro de Livros', () => {
             .send(livroValido);
 
             expect(resposta.status).to.equal(201);
-            expect(resposta.body.id_livro).to.be.a('string').and.to.equal('1A');
-            expect(resposta.body.nome).to.be.a('string').and.to.equal('Livro de Teste');
-            expect(resposta.body.autores).to.be.an('array').of.strings().that.includes('Autor 1', 'Autor 2');
-            expect(resposta.body.ano_publicacao).to.be.a('number').and.to.equal(2020);
-            expect(resposta.body.edicao).to.be.a('number').and.to.equal(1);
-            expect(resposta.body.paginas).to.be.a('number').and.to.equal(300);
-            expect(resposta.body.qtde_disponivel).to.be.a('number').and.to.equal(5);
-            expect(resposta.body.categoria).to.be.an('array').that.includes('Ficção');
-            expect(resposta.body.editora).to.be.a('string').and.to.equal('Editora Teste');
-            expect(resposta.body.idioma).to.be.a('string').and.to.equal('Português');
+            expect(resposta.body).to.be.deep.equal(livroValido);
         });
 
-        it('Tenta criar um registro com dados inválidos ou ausentes e retorna 400 ', async () => {
-            const livroInvalido = { ...livro, autores: '' }; // Deveria ter no mínimo um autor
+        it('Tenta cadastrar um novo livro com dados inválidos ou ausentes e retorna 400 ', async () => {
+            const livroInvalido = { ...livroCadastrado, autores: '' }; // Deveria ter no mínimo um autor
                         
             const resposta = await request(getApp())
             .post('/livros')
@@ -62,9 +54,9 @@ describe('Testes de Cadastro de Livros', () => {
             expect(resposta.body.erro).to.equal('Dados inválidos');
         });
 
-        it('Tenta criar um registro sem token de validação do usuário admin e retorna 401 ', async () => {
+        it('Cadastra um novo livro sem token de validação do usuário admin e retorna 401 ', async () => {
             const tokenInvalido = ''; // Deveria ter o token de autenticação do usuário admin
-            const livroValido = { ...livro, id_livro: '401A' };
+            const livroValido = { ...livroCadastrado, id_livro: '401A' };
             
             const resposta = await request(getApp())
             .post('/livros')
@@ -74,15 +66,9 @@ describe('Testes de Cadastro de Livros', () => {
             expect(resposta.status).to.equal(401);
         });
 
-        it('Tenta criar um registro duplicado e retorna 409 ', async () => {
-            const livroDuplicado = { ...livro, id_livro: 'DUP01' };
-
-            const criacao = await request(getApp())
-            .post('/livros')
-            .set('Authorization', `Bearer ${token}`)
-            .send(livroDuplicado);
-
-            expect(criacao.status).to.equal(201);
+        
+        it('Cadastra um novo livro duplicado e retorna 409 ', async () => {
+            const livroDuplicado = { ...livroCadastrado }; // ID já cadastrado no sistema
             
             const resposta = await request(getApp())
             .post('/livros')
@@ -97,26 +83,16 @@ describe('Testes de Cadastro de Livros', () => {
 
     describe('GET /Livros/{id_livro}', () => {
         it('Busca um livro específico por ID e retorna 200 ', async () => {
-            const livroValido = { ...livro, id_livro: '200GET', nome: 'Livro GET 200' };
-
-            const criacao = await request(getApp())
-            .post('/livros')
-            .set('Authorization', `Bearer ${token}`)
-            .send(livroValido);
-
-            expect(criacao.status).to.equal(201);
-
             const resposta = await request(getApp())
-            .get('/livros/200GET')
-            .set('Authorization', `Bearer ${token}`)
-            .send(livroValido.id_livro);
+            .get('/livros/10STD')
+            .send(livroCadastrado.id_livro);
 
             expect(resposta.status).to.equal(200);
-            expect(resposta.body.id_livro).to.equal('200GET');
+            expect(resposta.body.id_livro).to.equal('10STD');
         });
 
         it('Busca um livro específico com ID incorreto e retorna 404 ', async () => {
-            const livroInexistente = {...livro, id_livro: '999'}; // ID que não existe no sistema
+            const livroInexistente = {...livroCadastrado, id_livro: '999'}; // ID que não existe no sistema
             const resposta = await request(getApp())
             .get('/livros/999')
             .set('Authorization', `Bearer ${token}`)
@@ -128,18 +104,9 @@ describe('Testes de Cadastro de Livros', () => {
 
     describe('PUT /Livros/{id_livro}', () => {
         it('Atualiza um livro específico por ID e retorna 200 ', async () => {
-            const livroValido = { ...livro, id_livro: '200PUT', nome : 'Livro PUT 200' };
-
-            const criacao = await request(getApp())
-            .post('/livros')
-            .set('Authorization', `Bearer ${token}`)
-            .send(livroValido);
-
-            expect(criacao.status).to.equal(201);
-
-            const livroAtualizado = { ...livro, nome: 'Livro de Teste Atualizado' };
+            const livroAtualizado = { ...livroCadastrado, nome: 'Livro de Teste Atualizado' };
             const resposta = await request(getApp())
-            .put('/livros/200PUT')
+            .put('/livros/10STD')
             .set('Authorization', `Bearer ${token}`)
             .send(livroAtualizado);
 
@@ -148,40 +115,29 @@ describe('Testes de Cadastro de Livros', () => {
         });
 
         it('Tenta atualizar um livro com ID incorreto e retorna 404 ', async () => {
-            const livroAtualizado = { ...livro, nome: 'Livro de Teste Atualizado' };
             const resposta = await request(getApp())
             .put('/livros/999')
             .set('Authorization', `Bearer ${token}`)
-            .send(livroAtualizado);
+            .send(livroCadastrado);
 
             expect(resposta.status).to.equal(404);
         });
 
         it('Tenta atualizar um livro sem token de autenticação e retorna 401 ', async () => {
-            const livroAtualizado = { ...livro, nome: 'Livro de Teste Atualizado' };
             const tokenInvalido = ''; // Deveria ter o token de autenticação do usuário admin
             
             const resposta = await request(getApp())
-            .put('/livros/1A')
+            .put('/livros/10STD')
             .set('Authorization', `Bearer ${tokenInvalido}`)
-            .send(livroAtualizado);
+            .send(livroCadastrado);
 
             expect(resposta.status).to.equal(401);
         });
 
         it('Tenta atualizar um livro com dados inválidos e retorna 400 ', async () => {
-            const livroValido = { ...livro, id_livro: '400PUT', nome: 'Livro PUT 400' };
-
-             const criacao = await request(getApp())
-            .post('/livros')
-            .set('Authorization', `Bearer ${token}`)
-            .send(livroValido);
-
-            expect(criacao.status).to.equal(201);
-
             const livroAtualizado = { ...livro, nome: '' }; // Nome não pode ser vazio
             const resposta = await request(getApp())
-            .put('/livros/400PUT')
+            .put('/livros/10STD')
             .set('Authorization', `Bearer ${token}`)
             .send(livroAtualizado);
 
@@ -191,27 +147,19 @@ describe('Testes de Cadastro de Livros', () => {
 
         
         it('Tenta atualizar um livro para um ID já existente e retorna 409 ', async () => {
-            const livroAtualizadoA = { ...livro, id_livro: '409PUTA', nome: 'Livro PUT 409A' };
-            const livroAtualizadoB = { ...livro, id_livro: '409PUTB', nome: 'Livro PUT 409B' };
+            const livroAtualizadoA = { ...livro, id_livro: '409PUT', nome: 'Livro PUT 409A' };
                                                                                                 
-            const criacaoA = await request(getApp())
+            const criacao = await request(getApp())
             .post('/livros')
             .set('Authorization', `Bearer ${token}`)
             .send(livroAtualizadoA);
 
-            expect(criacaoA.status).to.equal(201);
-
-             const criacaoB = await request(getApp())
-            .post('/livros')
-            .set('Authorization', `Bearer ${token}`)
-            .send(livroAtualizadoB);
-
-            expect(criacaoB.status).to.equal(201);
+            expect(criacao.status).to.equal(201);
 
             const resposta = await request(getApp())                                             
-            .put('/livros/409PUTB')
+            .put('/livros/409PUT')
             .set('Authorization', `Bearer ${token}`)
-            .send(livroAtualizadoA);
+            .send(livroCadastrado);
 
             expect(resposta.status).to.equal(409);
             expect(resposta.body.erro).to.equal('Conflito');
@@ -220,14 +168,14 @@ describe('Testes de Cadastro de Livros', () => {
 
     describe('DELETE /Livros/{id_livro}', () => {
         it('Deleta um livro específico por ID e retorna 204 ', async () => {
-            const livroParaExcluir = { ...livro, id_livro: 'DEL001', nome: 'Livro DELETE 204'};
+            const livroParaDeletar = { ...livroCadastrado, id_livro: 'DEL001', nome: 'Livro para Deletar' };
             const criacao = await request(getApp())
             .post('/livros')
             .set('Authorization', `Bearer ${token}`)
-            .send(livroParaExcluir);
+            .send(livroParaDeletar);
 
             expect(criacao.status).to.equal(201);
-
+            
             const resposta = await request(getApp())
             .delete('/livros/DEL001')
             .set('Authorization', `Bearer ${token}`);
@@ -245,17 +193,9 @@ describe('Testes de Cadastro de Livros', () => {
 
         it('Tenta deletar um livro sem token de autenticação e retorna 401 ', async () => {
             const tokenInvalido = ''; // Deveria ter o token de autenticação do usuário admin
-            const livroSemAutenticacao = { ...livro, id_livro: 'DEL401', nome: 'Livro DELETE 401' };
            
-            const criacao = await request(getApp())
-            .post('/livros')
-            .set('Authorization', `Bearer ${token}`)
-            .send(livroSemAutenticacao);
-
-            expect(criacao.status).to.equal(201);
-            
             const resposta = await request(getApp())
-            .delete('/livros/DEL401')
+            .delete('/livros/10STD')
             .set('Authorization', `Bearer ${tokenInvalido}`);
 
             expect(resposta.status).to.equal(401);
