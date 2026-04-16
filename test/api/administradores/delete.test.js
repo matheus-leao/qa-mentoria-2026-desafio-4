f75a1bade8d125a11b10a445b3b2d3246dec43b1
Matheus Leao <matsouzaleao@gmail.com>
2026-04-15 23:37:32 -0300

    Fix delete test

--- test/api/administradores/delete.test.js
+++ test/api/administradores/delete.test.js
@@ -1,16 +1,29 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import { getApp } from '../../infra/helpers/appBuilder.js';
-import { postLogin } from '../../infra/helpers/autenticacao.js';
+import { faker } from '@faker-js/faker';
+import { criaUsuarioAdmin } from '../../infra/helpers/criaUsuarioAdmin.js';
+import { obterToken } from '../../infra/helpers/autenticacao.js';
 
 describe('DELETE /administradores/:id', () => {
-    let responseLogin;
-    beforeEach(async () => {
-        responseLogin = await postLogin('Luana', 'Nascimento', 11, 'luana@example.com', '123456')    
-    })
+    let token;
+    let adminId;
 
-    it('Deve retornar 204', async () => {
-        const response = await request(getApp()).delete('/administradores/1')
+    const usuarioAdminFaker = {
+        "nome": faker.person.firstName(),
+        "sobrenome": faker.person.lastName(),
+        "id_funcionario": faker.number.int(),
+        "email": faker.internet.email(),
+        "senha": "senha123"
+    }
+
+    before(async () => {
+            adminId = (await criaUsuarioAdmin(getApp(), usuarioAdminFaker)).body.id;
+            token = await obterToken(getApp(), usuarioAdminFaker.id_funcionario, usuarioAdminFaker.senha);
+        });
+
+    it.only('Deve retornar 204', async () => {
+        const response = await request(getApp()).delete(`/administradores/${adminId}`)
         expect(response.status).to.equal(204)
     })
 })
