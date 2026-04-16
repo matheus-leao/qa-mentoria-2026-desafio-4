f749ee08d999b818dcdf6303d4b758f5c8050ead
Matheus Leao <matsouzaleao@gmail.com>
2026-04-16 20:25:16 -0300

    Add default url

--- test/infra/helpers/appBuilder.js
+++ test/infra/helpers/appBuilder.js
@@ -2,5 +2,5 @@ import app from "../../../app.js";
 import { config } from "dotenv";
 
 export function getApp() {
-  return process.env.baseUrl;
+  return process.env.baseUrl || "http://localhost:3000";
 }
