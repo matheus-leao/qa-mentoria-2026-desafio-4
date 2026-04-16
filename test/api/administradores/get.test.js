b5df30ddee2ab86dcde0eb3e25980cbd0a9cc503
Matheus Leao <matsouzaleao@gmail.com>
2026-04-16 19:07:45 -0300

    Refactor get test

--- test/api/administradores/get.test.js
+++ test/api/administradores/get.test.js
@@ -2,29 +2,35 @@ import request from 'supertest';
 import { expect } from 'chai';
 import { getApp } from '../../infra/helpers/appBuilder.js';
 import { postLogin } from '../../infra/helpers/autenticacao.js';
+import { faker } from '@faker-js/faker';
+import { criaUsuarioAdmin } from '../../infra/helpers/criaUsuarioAdmin.js';
+import { obterToken } from '../../infra/helpers/autenticacao.js';
 
-describe('GET /administradores', () => {
-    let responseLogin;
+describe('GET /administradores/:id', () => {
 
-    beforeEach(async () => {
-        responseLogin = await postLogin('Luana', 'Nascimento', 11, 'luana@example.com', '123456')    
+    let token;
+    let adminId;
+
+    const usuarioAdminFaker = {
+        "nome": faker.person.firstName(),
+        "sobrenome": faker.person.lastName(),
+        "id_funcionario": faker.number.int(),
+        "email": faker.internet.email(),
+        "senha": "senha123"
+    }
+
+    before(async () => {
+            adminId = (await criaUsuarioAdmin(getApp(), usuarioAdminFaker)).body.id;
+            token = await obterToken(getApp(), usuarioAdminFaker.id_funcionario, usuarioAdminFaker.senha);
+        });
+
+    it('Deve retornar 200 contendo um administrador específico', async () => {
+        const response = await request(getApp()).get(`/administradores/${adminId}`)
+        expect(response.status).to.equal(200)
     })
 
     it('Deve retornar 200 contendo uma lista dos administradores cadastrados', async () => {
         const response = await request(getApp()).get('/administradores')
-        expect(response.status).to.equal(200)
-    })
-})
-
-describe('GET /administradores/:id', () => {
-    let responseLogin;
-
-    beforeEach(async () => {
-        responseLogin = await postLogin('Luana', 'Nascimento', 11, 'luana@example.com', '123456')    
-    })
-
-    it('Deve retornar 200 contendo um administrador específico', async () => {
-        const response = await request(getApp()).get('/administradores/2')
         expect(response.status).to.equal(200)
     })
 })
